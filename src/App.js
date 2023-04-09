348b87c567e97002314ac78e15e890bf0e8afa4f
vinay <vinayrayavarapu8729@example.com>
2023-04-09 15:42:00 +0530

    your message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 const App =  () => { 
-  const [name, setName] = useState(0)
-  useEffect(() => console.log('clicked'),[])
+  
 
   return (
     <div>
-    <p>clikced {name}</p>
-    
-    <button onClick={() => setName(name + 1)}>click me </button>
+      <h1>hello</h1>
     </div>
   )
 }
